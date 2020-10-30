9925ba72020a6aab0ce3ed4a40268045b3073efc
baoc2000 <55167505+baoc2000@users.noreply.github.com>
2020-10-30 18:47:13 -0300

    Add files via upload

--- Tarea 1/Benjamin/main.cpp
+++ Tarea 1/Benjamin/main.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
 #include <stdlib.h>
 #include "cqueue.h"
+#include "alist.h"
 
 using namespace std;
 
 int main() {
 	cqueue Q;
-
 	Q.enQueue(2, 1);
 	int arr[2];
 	Q.deQueue(arr);
 	cout << arr[0] << endl;
+
+	alist L;
+	L.append(2, 1);
+	if (L.isIn(2, 1)) cout << "Esta" << endl;
+	else cout << "No esta" << endl;
+	if (L.isIn(1, 1)) cout << "Esta" << endl;
+	else cout << "No esta" << endl;
 	return 0;
 }
