bc981e64686f26cfab5e91ba25f2ea3224d036b0
_MaxX_X <MaxGGx@users.noreply.github.com>
2020-11-02 12:25:10 -0300

    Update problema1 2.0
    Se modifican algunas partes de la funcion y se agregan returns para poder verificar si encontro solucion la funcion o no, se crea el menu para tomar entradas y luego imprimir los resultados

--- Tarea 1/Iñaki/problema1_2.0.py
+++ Tarea 1/Iñaki/problema1_2.0.py
@@ -22,14 +22,14 @@ def printSubs(lista,target):
 				break
 	if sum(listaP) == target and (listaP not in soluciones):	
 		soluciones.append(listaP)
-		print(listaP)
+		print("+".join(list(map(str,listaP))))
 
 def subSum(conjuntos,valores,Sconjuntos,Svalores,suma,node,target):
 	if target == suma:
 		printSubs(valores,target)
 		if(node+1 < Sconjuntos and suma-conjuntos[node]+conjuntos[node+1] <= target):
 			subSum(conjuntos,valores,Sconjuntos,Svalores-1,suma-conjuntos[node],node+1,target)
-		return
+		return 0
 	else:
 		if node < Sconjuntos and suma+conjuntos[node] <= target:
 			i = node
@@ -50,8 +50,35 @@ def generateSubs(conjuntos,Sconjuntos,target):
 		i += 1
 	if conjuntos[0] <= target and total >= target:
 		subSum(conjuntos,valores,Sconjuntos,0,0,0,target)
+		return 0
 
-items = [6,4,3,3,3,2,2,2]
-target = 6
+print("  _______                     __ ")                        
+print(" |__   __|                   /_ |")                       
+print("    | | __ _ _ __ ___  __ _   | |")                       
+print("    | |/ _` | '__/ _ \/ _` |  | |")                       
+print("    | | (_| | | |  __/ (_| |  | |")                       
+print("    |_|\__,_|_|  \___|\__,_|  |_|")                       
+print("     /\   | |                (_) |")                      
+print("    /  \  | | __ _  ___  _ __ _| |_ _ __ ___   ___  ___ ")
+print("   / /\ \ | |/ _` |/ _ \| '__| | __| '_ ` _ \ / _ \/ __|")
+print("  / ____ \| | (_| | (_) | |  | | |_| | | | | | (_) \__ \ ")
+print(" /_/    \_\_|\__, |\___/|_|  |_|\__|_| |_| |_|\___/|___/")
+print("              __/ |                                     ")
+print("             |___/                                      ")
 
-generateSubs(items,len(items),target)
+inputs = []
+
+while(True):
+	string = input("Ingrese entradas:\n>")
+	if string.split(" ")[1] == '0':
+		break
+	inputs.append(list(map(int,string.split(" "))))
+
+for entrada in inputs:
+	items=[]
+	target = entrada[0]
+	for x in range(2,entrada[1]+2):
+		items.append(entrada[x])
+	print("Suma de "+str(target)+":")
+	if generateSubs(items,len(items),target) == None:
+		print("NADA")
