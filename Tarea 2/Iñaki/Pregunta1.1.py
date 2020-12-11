9b34bb1b51a906473139ebd45f8aa7c8655d32aa
_MaxX_X <MaxGGx@users.noreply.github.com>
2020-12-10 21:29:09 -0300

    Update Pregunta 1.1
    Se cambian los parametros del acercamiento realizado y a la vez se deja el espacio para colar las explicaciones del código

--- Tarea 2/Iñaki/Pregunta1.1.py
+++ Tarea 2/Iñaki/Pregunta1.1.py
@@ -1,67 +1,50 @@
-# Python3 program to implement 
-# the above approach 
+
+'''
+
+'''
+def chequeoSubsec(valores, tamanio, mitad, bombas ): 
   
-# Function to check a subsequence can 
-# be formed with min difference mid 
-def can_place(A, n, B, mid): 
-  
-    count = 1
-    last_position = A[0] 
-  
-    # If a subsequence of size B 
-    # with min diff = mid is possible 
-    # return true else false 
-    for i in range(1, n): 
-        if (A[i] - last_position >= mid): 
-            last_position = A[i] 
-            count = count + 1
+    contador = 1
+    ulti = valores[0]
+   
+    for i in range(1, tamanio): 
+        if (valores[i] - ulti >= mitad): 
+            ulti = valores[i] 
+            contador = contador + 1
               
-            if (count == B): 
-                return bool(True) 
+            if (contador == bombas): 
+                return True
                   
-    return bool(False) 
+    return False 
   
 # Function to find the maximum of 
 # all minimum difference of pairs 
-# possible among the subsequence 
-def find_min_difference(A, n, B): 
-  
-    # Sort the Array 
-    A.sort() 
-  
-    # Stores the boundaries 
-    # of the search space 
-    s = 0
-    e = A[n - 1] - A[0]
-    #print(e) 
-  
-    # Store the answer 
-    ans = 0
-  
-    # Binary Search 
-    while (s <= e): 
-        mid = (int)((s + e) / 2) 
-        print(mid)
+# possible among the subsequence
+'''
+
+''' 
+def minDif(valores, tamanio, bombas): 
+	sol = 0  
+	inicio = 0
+	fin = valores[-1] - valores[0]
+     
+	while (inicio <= fin): 
+		mitad = (int((inicio + fin) / 2)) 
         # If subsequence can be formed 
-        # with min diff mid and size B 
-        if (can_place(A, n, B, mid)): 
-            ans = mid 
-  
-            # Right half 
-            s = mid + 1
-          
-        else: 
-  
-            # Left half 
-            e = mid - 1
+        # with min diff mid and size bombas 
+		if (chequeoSubsec(valores, tamanio, mitad, bombas)): 
+			sol = mitad 
+			inicio = mitad + 1     
+		else:  
+			fin = mitad - 1
       
-    return ans 
+	return sol 
   
-# Driver code 
-A = [ 1, 2, 4, 8, 9 ] 
-n = len(A) 
-B = 3
+# Valores de entrada
+valores = [ 1, 2, 4, 8, 9 ] 
+tamanio = len(valores) 
+bombas = 3
   
-min_difference = find_min_difference(A, n, B) 
+minimo = minDif(valores, tamanio, bombas) 
   
-print(min_difference) 
+print(minimo) 
